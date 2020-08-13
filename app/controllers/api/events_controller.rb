4cbbbf3babc9a6afa97b524380d1ce987b6b1cce
ALEJANDRA SABOGAL <ja.sabogal@uniandes.edu.co>
2020-08-13 14:47:28 -0500

    Was developed create, put and delete methods

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -43,14 +43,71 @@ module Api
 			else
 				render json:{status: 'ERROR', message:'Token invalid'}, status: :unprocessable_entity
 			end			
-        end
-        
+		end
+		
+		def create
+			@user = User.where(token: request.headers["Authorization"]).first
+			@category = Category.where(name:  params[:event_category]).first
+			@ev_type = EventType.where(name: params[:event_typed]).first
+			newEvent = Event.create(
+				name: params[:name],
+				location: params[:location],
+				address: params[:address],
+				initial_date: params[:initial_date],
+				final_date: params[:final_date],
+				thumbnail: params[:thumbnail],
+				user_id: @user.id,
+				category_id: @category.id,
+				event_type_id: @ev_type.id
+			)
+
+			if newEvent.save
+				render json: newEvent.to_json()
+			else
+				render json:{status: 'ERROR', message:'Event not saved'}, status: :unprocessable_entity
+			end
+		end
+
+		def update
+			@user = User.where(token: request.headers["Authorization"]).first
+			@category = Category.where(name:  params[:event_category]).first
+			@ev_type = EventType.where(name: params[:event_typed]).first
+			newEvent = Event.update(
+				id: params[:id],
+				name: params[:name],
+				location: params[:location],
+				address: params[:address],
+				initial_date: params[:initial_date],
+				final_date: params[:final_date],
+				thumbnail: params[:thumbnail],
+				user_id: @user.id,
+				category_id: @category.id,
+				event_type_id: @ev_type.id
+			)
+
+			if newEvent.save
+				render json: newEvent.to_json()
+			else
+				render json:{status: 'ERROR', message:'Event not saved'}, status: :unprocessable_entity
+			end
+		end
+						
+		def destroy
+			@user = User.where(token: request.headers["Authorization"]).first
+			event = Event.where(events: {user_id: @user.id}).where(events: {id: params[:id]})
+			if !@user.to_s.strip.empty?
+				event.delete_all
+				render json:{status: 'SUCCES', message:'Event deleted'}
+			else
+				render json:{status: 'ERROR', message:'Event not exist'}, status: :unprocessable_entity
+			end	
+		end
+
 		private
 
 		def event_params
 			params.permit(
 				:name,
-				:idCategory,
 				:location,
 				:address,
 				:initial_date,
