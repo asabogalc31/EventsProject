e5c9dcbddcf1954144d18309a837df27df0f4eb3
ALEJANDRA SABOGAL <ja.sabogal@uniandes.edu.co>
2020-08-14 11:22:17 -0500

    permissions access from any ip address

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,7 @@ module EventsProject
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.web_console.whitelisted = '0.0.0.0/0'
+    config.web_console.whiny_requests = false
   end
 end
