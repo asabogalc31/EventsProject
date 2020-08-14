2e8f504af8cc70ed35ec82b3aac2f337d4fa341a
ALEJANDRA SABOGAL <ja.sabogal@uniandes.edu.co>
2020-08-13 22:50:52 -0500

    Permissions ips

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
-  config.web_console.whiny_requests = false
+  config.web_console.whitelisted_ips = '157.253.238.244'
 end
